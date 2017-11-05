7ad65f86c62ab5fc0abedd05c0bac2eaeeafd6a0
Ahmad Anvari <ahmad.anv@gmail.com>
2017-11-05 13:19:33 +0330

    Add lexeme struct

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -5,6 +5,18 @@
 #ifndef RAZPILER_LEXER_H
 #define RAZPILER_LEXER_H
 
+#include "scanner.h"
+
+typedef struct Lexeme
+{
+    wchar_t *value;
+    unsigned char type;
+    long long column;
+    FileLine *file_line;
+    int symbol_table_id;
+    struct Lexeme *next;
+}Lexeme;
+
 int func();
 
 #endif //RAZPILER_LEXER_H
