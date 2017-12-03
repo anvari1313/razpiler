6209b0ecdc663fe185203c5ddde9544c8691cd1d
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-03 21:39:14 +0330

    LLStack is implemented :D but not yet complete

---
+++ src/util/llstack/llstack.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <memory.h>
+#include "llstack.h"
+
+void llstack_init(LLStack *llstack)
+{
+    (*llstack) = malloc(sizeof(LLStack));
+    (*llstack)->__start_node = (*llstack)->__last_node = NULL;
+    (*llstack)->size = 0;
+}
+
+void llstack_push(LLStack llstack, LLStackData llstack_data)
+{
+    if (llstack->__start_node == NULL)          // Stack is empty
+    {
+        LLStackNode node = malloc(sizeof(LLStackNodeType));
+//        memcpy(node->__data, llstack_data, sizeof((llstack_data)));
+        node->__data = llstack_data;
+        node->__ll_last = NULL;
+        llstack->__start_node = llstack->__last_node = node;
+    }
+    else
+    {
+        LLStackNode node = malloc(sizeof(LLStackNodeType));
+//        memcpy(node->__data, llstack_data, sizeof((llstack_data)));
+        node->__data = llstack_data;
+        node->__ll_last = llstack->__last_node;
+        llstack->__last_node = node;
+    }
+
+    llstack->size ++;
+}
+
+void llstack_pop(LLStack llstack)
+{
+    if (llstack->__start_node != NULL)
+    {
+        free(llstack->__last_node->__data);     // Free the data section
+        if (llstack->__last_node == llstack->__start_node)
+        {
+            free(llstack->__last_node);
+            llstack->__start_node = llstack->__last_node = NULL;
+        }
+        else
+        {
+            LLStackNode last_node = llstack->__last_node->__ll_last;
+            free(llstack->__last_node);
+            llstack->__last_node = last_node;
+        }
+
+        llstack->size --;
+    }
+}
+
+LLStackData llstack_top(LLStack llstack)
+{
+    return llstack->__last_node->__data;
+}
+
+LLStackData llstack_top_pop(LLStack llstack)
+{
+    // TODO implement this part
+    return NULL;
+}
+
+void llstack_release(LLStack llstack)
+{
+    // TODO implement this part
+}
+
+bool llstack_is_empty(LLStack llstack) {
+    return ((llstack->__last_node == llstack->__start_node) && (llstack->__last_node == NULL)) || (llstack->size == 0);
+}
---
+++ src/util/llstack/llstack.h
@@ -0,0 +1,69 @@
+/**
+ * Linked list implementation of Stack
+ * General purpose with void
+ */
+#ifndef RAZPILER_LLSTACK_H
+#define RAZPILER_LLSTACK_H
+
+#include <stdbool.h>
+
+typedef void *LLStackData;
+
+typedef struct LLStackNode{
+    LLStackData __data;
+    struct LLStackNode *__ll_last;
+} *LLStackNode, LLStackNodeType;
+
+typedef struct {
+    LLStackNode __start_node;
+    LLStackNode __last_node;
+    long long size;
+} *LLStack;
+
+/**
+ * Initial the data structure of stack container
+ * @param llstack The stack container
+ */
+void llstack_init(LLStack *);
+
+/**
+ * Push action in the stack
+ * @param llstack The stack to be pushed to
+ * @param llstack_data The data that should be pushed to the stack
+ */
+void llstack_push(LLStack, LLStackData);
+
+/**
+ * Pop action from the stack
+ * @param llstack The stack that should be popped from
+ */
+void llstack_pop(LLStack);
+
+/**
+ * Top element of the stack
+ * @param llstack The stack that the top element is needed
+ * @return The top element of the stack
+ */
+LLStackData llstack_top(LLStack);
+
+/**
+ * Get the top element of the stack and pop it!
+ * @param llstack The stack that top_pop is needed
+ * @return The top element of the stack
+ */
+LLStackData llstack_top_pop(LLStack);
+
+/**
+ * Returns whether the stack is empty or not
+ * @param llstack The stack to be checked if it is empty or not
+ * @return The emptiness of the stack
+ */
+bool llstack_is_empty(LLStack);
+
+/**
+ * Release the resources allocated to the llstack
+ * @param llstack The llstack that should be un allocate from the memory
+ */
+void llstack_release(LLStack);
+
+#endif //RAZPILER_LLSTACK_H
