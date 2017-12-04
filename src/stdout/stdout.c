7da79eb2cb1373cda6558b762655d59dcb54665d
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-04 23:11:51 +0330

    Adding std out module

---
+++ src/stdout/stdout.c
@@ -0,0 +1,21 @@
+#include "stdout.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void std_out(const unsigned char type, const unsigned char error)
+{
+    switch (error)
+    {
+        case ERROR_FILE_NOT_FOUND:
+            printf("File not found\n");
+            break;
+    }
+
+    switch (type)
+    {
+        case ERROR_TYPE_CRITICAL:
+            exit(-1);
+        default:
+            break;
+    }
+}
---
+++ src/stdout/stdout.h
@@ -0,0 +1,12 @@
+#ifndef RAZPILER_STDOUT_H
+#define RAZPILER_STDOUT_H
+
+// Error Types
+#define ERROR_TYPE_CRITICAL 255
+
+// Errors
+#define ERROR_FILE_NOT_FOUND 255
+
+void std_out(const unsigned char type, const unsigned char error);
+
+#endif //RAZPILER_STDOUT_H
