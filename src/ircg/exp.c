104cfd0ae0147543cd3ad6d4df10b96db336d01e
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-08 10:54:52 +0330

    Adding expression struct

---
+++ src/ircg/exp.c
@@ -0,0 +1,16 @@
+#include "exp.h"
+
+#include <stdlib.h>
+
+#if defined(unix) || defined(__unix__) || defined(__unix)
+#include <stdlib.h>     // Should be activated in linux
+#else
+#include <stdlib.h>     // Should be activated in MacOSX
+#endif
+
+void exp_init(Expression *expression)
+{
+    (*expression) = malloc(sizeof(ExpressionType));
+    lllist_init(&((*expression)->false_list));
+    lllist_init(&((*expression)->true_list));
+}
---
+++ src/ircg/exp.h
@@ -0,0 +1,17 @@
+#ifndef RAZPILER_EXP_H
+#define RAZPILER_EXP_H
+
+#include <string.h>
+#include "../util/lllist/lllist.h"
+
+typedef struct {
+    char *code;
+    LLList true_list;
+    LLList false_list;
+    char *place;
+    char *value;
+} *Expression, ExpressionType;
+
+void exp_init(Expression *);
+
+#endif //RAZPILER_EXP_H
