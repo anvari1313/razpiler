9504297d26aa6258989ee599761ebf84aa26c70c
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-15 20:56:11 +0330

    A basic symbol table is implemented

--- src/symtable/symtable.c
+++ src/symtable/symtable.c
@@ -1,2 +1,53 @@
 #include "symtable.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "kwtable.h"
 
+void init_symbol_table()
+{
+    lllist_init(&symbols_list);
+    kw_add("int");
+    kw_add("float");
+    kw_add("main");
+}
+
+SymbolNode install_id(char *id_name, unsigned char type)
+{
+    if (search_id(id_name) == NULL)
+    {
+        SymbolNode node = new_symbol(type, id_name);
+        lllist_push_front(symbols_list, node);
+        return node;
+    }
+    else
+    {
+        printf("%s is used somewhere else\n", id_name);
+        exit(-1);
+    }
+}
+
+SymbolNode search_id(char *id_name)
+{
+    lllist_go_last(symbols_list);
+    if (lllist_is_empty(symbols_list))
+        return NULL;
+
+    do
+    {
+        if (strcmp(((SymbolNode)lllist_get_current(symbols_list))->symbol_name, id_name) == 0)
+            return lllist_get_current_p(symbols_list);
+    }
+    while (lllist_step_backward(symbols_list) != false);
+
+    return NULL;
+}
+
+SymbolNode new_symbol(unsigned char symbol_type, char *symbol_name) {
+    SymbolNode symbol_node = malloc(sizeof(SymbolNodeType));
+    symbol_node->symbol_type = symbol_type;
+    symbol_node->symbol_name = malloc(strlen(symbol_name) * sizeof(char));
+    strcpy(symbol_node->symbol_name, symbol_name);
+
+    return symbol_node;
+}
--- src/symtable/symtable.h
+++ src/symtable/symtable.h
@@ -2,9 +2,24 @@
 #define RAZPILER_SYMTABLE_H
 
 #include "../util/llstack/llstack.h"
-
+#include "../util/lllist/lllist.h"
 
 typedef struct {
     LLStack function_stack;
 } *FunctionBlock;
+
+LLList symbols_list;
+
+typedef struct
+{
+    unsigned char symbol_type;
+    char *symbol_name;
+} *SymbolNode, SymbolNodeType;
+
+void init_symbol_table();
+
+SymbolNode install_id(char *id_name, unsigned char type);
+SymbolNode search_id(char *id_name);
+SymbolNode new_symbol(unsigned char symbol_type, char *symbol_name);
+
 #endif //RAZPILER_SYMTABLE_H
