189b2b1e8254ce4cd2b1a9b0ec4ef56115a25928
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-16 11:02:27 +0330

    Adding bash for compiling yacc and lex

---
+++ resource/run.sh
@@ -0,0 +1,8 @@
+# !/bin/sh
+
+echo "This is compiling"
+cd ../src/scanner/
+rm parser.tab.h parser.tab.c lex.yy.c
+flex --noyywrap ../../resource/lexer.l
+bison -d ../../resource/parser.y
+# gcc parser.tab.h parser.tab.c lex.yy.c -lfl
