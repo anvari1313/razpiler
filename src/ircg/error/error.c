b8f2856b8a0f9271c74790d89c715c7f3b4c785d
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-16 22:13:25 +0330

    Adding error package

---
+++ src/ircg/error/error.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "error.h"
+
+void error_id_not_declared(char *id)
+{
+    printf("error : id not found\n");
+//    exit(-1);
+}
---
+++ src/ircg/error/error.h
@@ -0,0 +1,6 @@
+#ifndef RAZPILER_ERROR_H
+#define RAZPILER_ERROR_H
+
+void error_id_not_declared(char *);
+
+#endif //RAZPILER_ERROR_H
