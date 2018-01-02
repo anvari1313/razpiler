7b5c70052a242955dd2db7c7ece2d0211b1a64c6
Ahmad Anvari <ahmad.anv@gmail.com>
2018-01-02 21:30:04 +0330

    Adding file convertor

--- resource/run.sh
+++ resource/run.sh
@@ -6,3 +6,5 @@ rm parser.tab.h parser.tab.c lex.yy.c
 flex --noyywrap ../../resource/lexer.l
 bison -d ../../resource/parser.y
 # gcc parser.tab.h parser.tab.c lex.yy.c -lfl
+javac ../../resource/Converter.java
+mv ../../resource/Converter.class ../../cmake-build-debug/
