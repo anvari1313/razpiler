11c95dee0e169d3d76afc18be61671eeb9512d95
Ahmad Anvari <ahmad.anv@gmail.com>
2018-01-04 11:43:52 +0330

    Adding new quad functionality

--- src/ircg/quadruple/quad.c
+++ src/ircg/quadruple/quad.c
@@ -3,27 +3,57 @@
 #include <string.h>
 #include <stdio.h>
 
-LLList quad_list;
 
 void quad_init()
 {
     lllist_init(&quad_list);
+    quad_it = 0;
 }
 
-void quad_add(char *code)
+Quad quad_add(char *code)
 {
     lllist_go_last(quad_list);
-    char *tcode = (char *)malloc((strlen(code) + 1) * sizeof(char));
-    strcpy(tcode, code);
-    lllist_push_front(quad_list, tcode);
+    Quad q = malloc(sizeof(Quad_t));
+    q->index = quad_it++;
+
+    q->arg1 = (char *)malloc((strlen(code) + 1) * sizeof(char));
+    q->arg2 = (char *)malloc(1 * sizeof(char));
+    q->arg3 = (char *)malloc(1 * sizeof(char));
+    q->arg4 = (char *)malloc(1 * sizeof(char));
+    strcpy(q->arg1, code);
+    q->arg2[0] = q->arg3[0] = q->arg4[0] = '\0';
+
+    lllist_push_front(quad_list, q);
+
+    return q;
 }
 
-void print_quad()
+Quad quad_add4(char *arg1, char *arg2, char *arg3, char *arg4)
+{
+    lllist_go_last(quad_list);
+    Quad q = malloc(sizeof(Quad_t));
+    q->index = quad_it++;
+
+    q->arg1 = (char *)malloc((strlen(arg1) + 1) * sizeof(char));
+    q->arg2 = (char *)malloc((strlen(arg2) + 1) * sizeof(char));
+    q->arg3 = (char *)malloc((strlen(arg3) + 1) * sizeof(char));
+    q->arg4 = (char *)malloc((strlen(arg4) + 1) * sizeof(char));
+    strcpy(q->arg1, arg1);
+    strcpy(q->arg2, arg2);
+    strcpy(q->arg3, arg3);
+    strcpy(q->arg4, arg4);
+
+    lllist_push_front(quad_list, q);
+
+    return q;
+}
+void quad_print()
 {
     lllist_go_first(quad_list);
     do
     {
-        printf("%s\n", (char *)lllist_get_current(quad_list));
+        Quad q = (Quad)lllist_get_current(quad_list);
+        printf("%d : %s %s %s %s\n", q->index, q->arg1, q->arg2, q->arg3, q->arg4);
     }
     while (lllist_step_forward(quad_list) != false);
 }
--- src/ircg/quadruple/quad.h
+++ src/ircg/quadruple/quad.h
@@ -3,7 +3,21 @@
 
 #include "../../util/lllist/lllist.h"
 
+typedef struct {
+    char *arg1;
+    char *arg2;
+    char *arg3;
+    char *arg4;
+    int index;
+} *Quad, Quad_t;
+
+LLList quad_list;
+
+int quad_it;
+
 void quad_init();
-void quad_add(char *);
-void print_quad();
+Quad quad_add(char *);
+Quad quad_add4(char *, char *, char *, char *);
+
+void quad_print();
 #endif //RAZPILER_QUAD_H
